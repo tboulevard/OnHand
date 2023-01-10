83475f1d8bdad173fb930d8d618ed19f13115656
Tim Street <tstreet@tims-mbp.lan>
2023-01-10 15:54:37 -0800

    added model/network modules, added base ingredient search models

---
+++ core/data/src/main/java/com/tstreet/onhand/core/data/IngredientSearchRepository.kt
@@ -0,0 +1,11 @@
+package com.tstreet.onhand.core.data
+
+import com.tstreet.onhand.core.model.Ingredient
+
+class IngredientSearchRepository {
+
+    fun searchIngredients(prefix : String) : List<Ingredient> {
+        // TODO: Call to network layer
+        return listOf()
+    }
+}
---
+++ core/network/src/main/java/com/tstreet/onhand/core/network/model/NetworkIngredient.kt
@@ -0,0 +1,11 @@
+package com.tstreet.onhand.core.network.model
+
+import com.tstreet.onhand.core.model.ChildIngredient
+
+class NetworkIngredient(
+    val id: Int,
+    val name : String,
+    val image : String,
+    // TODO: Refactor if needed
+    val children : ChildIngredient? = null
+)
