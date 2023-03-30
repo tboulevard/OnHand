74b58eb83605e89fd9b4f1efec411bc2b4bd5847
tboulevard <ttboulevard@gmail.com>
2023-03-30 14:45:09 -0700

    mvp sorting logic with slight refactor for success/error/loading states

--- feature/recipesearch/src/main/java/com/tstreet/onhand/feature/recipesearch/RecipeSearchViewModel.kt
+++ feature/recipesearch/src/main/java/com/tstreet/onhand/feature/recipesearch/RecipeSearchViewModel.kt
@@ -4,11 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tstreet.onhand.core.common.CommonModule.IO
 import com.tstreet.onhand.core.domain.GetRecipesUseCase
-import com.tstreet.onhand.core.model.Recipe
+import com.tstreet.onhand.core.ui.RecipeSearchUiState
 import com.tstreet.onhand.feature.recipesearch.SortBy.*
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Named
 import javax.inject.Provider
@@ -23,33 +22,32 @@ class RecipeSearchViewModel @Inject constructor(
         println("[OnHand] Creating ${this.javaClass.simpleName}")
     }
 
-    private val _isLoading = MutableStateFlow(false)
-    val isLoading = _isLoading
-
-    private val _sortBy = MutableStateFlow(POPULARITY)
-    val recipes: StateFlow<List<Recipe>> = _sortBy
-        .onEach { _isLoading.update { true } }
-            // TODO: Each time we invoke use case it makes a network call - cache values on recipe screen in repo layer
+    private val _sortOrder = MutableStateFlow(DEFAULT_SORT_ORDER)
+    val recipeSearchUiState: StateFlow<RecipeSearchUiState> = _sortOrder
+        .onEach { RecipeSearchUiState.Loading }
+            // TODO: Each time we invoke use case it makes a network call - cache values on recipe
+            //  screen in repo layer (only re-query network if pantry state has changed)
         .combine(getRecipes.get().invoke()) { sortBy, recipes ->
-            // TODO: move logic into usecase layer
+            // TODO: move logic into usecase layer for better separation of concerns. Leaving
+            // here for v1
             when (sortBy) {
                 POPULARITY -> recipes.sortedByDescending { it.likes }
                 MISSING_INGREDIENTS -> recipes.sortedBy { it.missedIngredientCount }
             }
         }
-        .onEach { _isLoading.update { false } }
+        .map(RecipeSearchUiState::Success)
         .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = emptyList()
+            initialValue = RecipeSearchUiState.Loading
         )
 
     fun onRecipeSaved(index: Int) {
         /* TODO: implement */
     }
 
-    fun onSortingOrderChanged(sortingOrder: SortBy) {
-        _sortBy.update { sortingOrder }
+    fun onSortOrderChanged(sortingOrder: SortBy) {
+        _sortOrder.update { sortingOrder }
     }
 }
 
@@ -57,3 +55,5 @@ enum class SortBy {
     POPULARITY,
     MISSING_INGREDIENTS
 }
+
+val DEFAULT_SORT_ORDER = POPULARITY
