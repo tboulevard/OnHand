3243e3fa7a3484b5956dbc5e5df152abcc64fb39
tboulevard <ttboulevard@gmail.com>
2023-04-07 12:15:11 -0700

    mvp working

--- core/database/src/main/java/com/tstreet/onhand/core/database/dao/SavedRecipeDao.kt
+++ core/database/src/main/java/com/tstreet/onhand/core/database/dao/SavedRecipeDao.kt
@@ -3,7 +3,9 @@ package com.tstreet.onhand.core.database.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Transaction
 import com.tstreet.onhand.core.database.model.SavedRecipeEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SavedRecipeDao {
@@ -16,4 +18,8 @@ interface SavedRecipeDao {
 
     @Query("DELETE FROM saved_recipes WHERE id = :id")
     suspend fun deleteRecipe(id: Int)
+
+    @Query("SELECT * from saved_recipes WHERE id = :id")
+    @Transaction
+    fun getRecipe(id: Int) : Flow<SavedRecipeEntity>
 }
