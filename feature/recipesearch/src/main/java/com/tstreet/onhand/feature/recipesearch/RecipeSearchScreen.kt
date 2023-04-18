eea9a39476590ca8d74e6a32f050e32c055bc973
tboulevard <ttboulevard@gmail.com>
2023-04-17 20:49:47 -0700

    remove model logic, comment out row for later use

--- feature/recipesearch/src/main/java/com/tstreet/onhand/feature/recipesearch/RecipeSearchScreen.kt
+++ feature/recipesearch/src/main/java/com/tstreet/onhand/feature/recipesearch/RecipeSearchScreen.kt
@@ -1,12 +1,8 @@
 package com.tstreet.onhand.feature.recipesearch
 
-import android.graphics.drawable.VectorDrawable
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
@@ -14,9 +10,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
@@ -84,7 +77,6 @@ class RecipeSearchCard(
     val usedIngredients: Int,
     val missedIngredients: Int,
     val likes: Int,
-    val readyTime: Int = 30, //TODO: remove default value when we pass through real value
     val saveState: RecipeSaveState
 )
 
@@ -262,21 +254,21 @@ fun RecipeSearchCardItem(
                         style = MaterialTheme.typography.bodyMedium
                     )
                 }
-
-                Row(modifier = Modifier.padding(4.dp)) {
-                    Icon(
-                        painterResource(com.tstreet.onhand.core.ui.R.drawable.timer),
-                        contentDescription = "time",
-                        modifier = Modifier
-                            .size(18.dp)
-                            .padding(end = 4.dp),
-                        tint = MaterialTheme.colorScheme.inverseOnSurface
-                    )
-                    Text(
-                        text = "${card.readyTime} minutes",
-                        style = MaterialTheme.typography.bodyMedium
-                    )
-                }
+// TODO: where to put time if we decide to send through later
+//                Row(modifier = Modifier.padding(4.dp)) {
+//                    Icon(
+//                        painterResource(com.tstreet.onhand.core.ui.R.drawable.timer),
+//                        contentDescription = "time",
+//                        modifier = Modifier
+//                            .size(18.dp)
+//                            .padding(end = 4.dp),
+//                        tint = MaterialTheme.colorScheme.inverseOnSurface
+//                    )
+//                    Text(
+//                        text = "-- minutes",
+//                        style = MaterialTheme.typography.bodyMedium
+//                    )
+//                }
 
             }
             Column(
