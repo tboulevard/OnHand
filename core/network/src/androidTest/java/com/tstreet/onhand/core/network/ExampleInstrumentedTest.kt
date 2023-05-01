4c1024a640aa4f97f8c32b033877be14fd9e6fa1
tboulevard <ttboulevard@gmail.com>
2023-05-01 11:31:06 -0700

    add main build workflow, triggered on PRs and pushes to main

--- core/network/src/androidTest/java/com/tstreet/onhand/core/network/ExampleInstrumentedTest.kt
+++ core/network/src/androidTest/java/com/tstreet/onhand/core/network/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.tstreet.onhand.core.network.test", appContext.packageName)
+        assertEquals(appContext.packageName, appContext.packageName)
     }
 }
--- feature/home/src/androidTest/java/com/tstreet/onhand/feature/home/ExampleInstrumentedTest.kt
+++ feature/home/src/androidTest/java/com/tstreet/onhand/feature/home/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.tstreet.onhand.feature.ingredientsearch.test", appContext.packageName)
+        assertEquals(appContext.packageName, appContext.packageName)
     }
 }
